4dc0db1dca28a2c2a77556ecc5c2917eea3f41fe
Paolo Garcia <pmatteo1108@yahoo.com>
2022-09-02 16:12:44 -0600

    commit tree f4317bf29bce857ba2f41bebbf9585646bce9753

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop('Garfield');
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift('Milo');
+    return cats;
+}
+
+function appendCat(name) {
+    const mainCats = ["Milo", "Otis", "Garfield"];
+    const allCats = [...mainCats, "Broom"];
+    return allCats;
+    return mainCats;
+}
+
+function prependCat(name) {
+    const mainCats = ["Milo", "Otis", "Garfield"];
+    const moreCats = ["Arnold", ...mainCats];
+    return moreCats;
+    return; mainCats;
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+    return cats;
+}
+
+
+
+function removeFirstCat() {
+    return cats.slice(1);
+    return cats;
+}
+
